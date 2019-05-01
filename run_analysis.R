3b4b44c314c2c1c8fc9249b46ad6502c5e7e4c25
Conrado Hernán Villa Gil <conradovilla@gmail.com>
2019-05-01 18:10:46 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,7 @@
 setwd("~/Desktop/JhonHopkins/Data Sets/Human Activity/UCI HAR Dataset")
 library(dplyr)
 
+#1.Read Tables
 features <- read.table("features.txt", col.names = c("n","functions"))
 activities <- read.table("activity_labels.txt", col.names = c("code", "activity"))
 subject_test <- read.table("test/subject_test.txt", col.names = "subject")
@@ -10,7 +11,7 @@ subject_train <- read.table("train/subject_train.txt", col.names = "subject")
 x_train <- read.table("train/X_train.txt", col.names = features$functions)
 y_train <- read.table("train/y_train.txt", col.names = "code")
 
-
+#2. Merge Tables
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
@@ -20,6 +21,7 @@ TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("st
 
 TidyData$code <- activities[TidyData$code, 2]
 
+#3 and 4 : Names of variables
 names(TidyData)[2] = "activity"
 names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
 names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
@@ -34,6 +36,8 @@ names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRU
 names(TidyData)<-gsub("angle", "Angle", names(TidyData))
 names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
 
+
+# 5. Final Data
 FinalData <- TidyData %>%
   group_by(subject, activity) %>%
   summarise_all(funs(mean))
